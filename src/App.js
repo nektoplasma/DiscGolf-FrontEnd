46de0203d760980c2030915b2958c6f0654da80b
Tomi Neste <tomi.neste@gmail.com>
2018-02-21 20:08:42 +0200

    Added React+Material-UI

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const App = () => (
+    <MuiThemeProvider>
+        <RaisedButton label="Default" />
+    </MuiThemeProvider>
+);
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('App')
+);
